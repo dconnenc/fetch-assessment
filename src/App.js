611db45e35f755f06e9e2f378fe3a829a9b75eb6
dconnenc <dconnenc@gmail.com>
2023-01-20 16:26:21 -0600

    Form Skeleton
    Form Options should be correctly implemented.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   
   const [occupations, setOccupations] = useState({});
   const [states, setStates] = useState({});
-  const [name, setName] = useState('')
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('')
+  
+  //fetches data for locations and occupations select
   const getFormData = async () => {
     try {
       const response = await fetch('https://frontend-take-home.fetchrewards.com/form')
@@ -18,31 +21,69 @@ function App() {
       console.log("There was an error:", error)
     }
   }
-    
+
   useEffect(()=> {
     getFormData();
-    console.log(states, occupations)
   }, [])
 
   return (
     <div className="wrapper">
-      <div className="form centered">
+      <form className="form centered">
         <h1>Join the Fun!</h1>
-        <form >
-          <input type="text" value={name}placeholder="Full name..."/>
-          <textarea placeholder="Email..." />
-          <textarea placeholder="Password..." />
-          <textarea placeholder="Occupation..." />
-          <textarea placeholder="State..." />
-          <label>
-            <select>
-              {/*states?.map(x => {
-                <option value={`${x.name}, ${x.abbreviation}`} />
-              })*/}
+          <label>Full Name:
+            <br />  
+            <input  type="text" 
+                    value={name} 
+                    onChange={(e)=>{setName(e.target.value)}}
+                    placeholder="Full name..."
+                    alt="full-name-input" 
+                    required />
+          </label>
+          <label> Email: 
+            <br/>
+            <input  type="email"
+                    value={email} 
+                    onChange={(e)=>{setEmail(e.target.value)}}
+                    placeholder="email@emailaddress.com"
+                    alt="email-input" 
+                    required />
+          </label>
+          <label> Password: <br />
+            <input  type="text"
+                    placeholder="Password..." 
+                    required /><br />
+                  Confirm Password: <br />
+            <input  type="text"
+                    placeholder="Make sure it matches..." 
+                    required /><br /> 
+          </label>
+          <label> Occupation: <br />
+            <select placeholder="Occupation..." required >
+              { //hydrates occupation select with fetched data
+                occupations.length &&
+                  occupations?.map((job, index) => {
+                    return <option 
+                      alt={job} 
+                      key={index} 
+                      value={job}
+                      >{job}</option>
+              })}
             </select>
           </label>
-        </form>
-      </div>
+          <label> State: <br />
+            <select  required>
+              { states.length &&
+                  states.map((state, index) => {
+                    return <option
+                              alt={state.name}
+                              key={index} 
+                              value={`${state.name}, ${state.abbreviation}`}
+                              > {`${state.name}, ${state.abbreviation}`}
+                            </option>
+              })}
+            </select>
+          </label> 
+      </form>
     </div>
   );
 }
